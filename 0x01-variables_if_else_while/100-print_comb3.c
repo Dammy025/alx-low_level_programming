27e6b588c1d3d8f91c1e96856556f666a9650448
Dammy025 <modupeabimbola55@gmail.com>
2022-10-15 09:27:29 -0700

    Code

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,11 +9,18 @@ int main(void)
 
 	for (i = '0'; i <= '9'; i++)
 	{
+
 		for (j = k; j <= '9'; j++)
+		
 		{
 			if (i != j)
 			{
-				continue;
+				putchar(i);
+				putchar(j);
+			}
+			if (i == j)
+			{
+		continue;
 			}
 			if (i == '8' && j == '9')
 			{
