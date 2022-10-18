8fd99c2979e6d2b0569c8f7fdeb3fb3fdeeadcf4
Dammy025 <modupeabimbola55@gmail.com>
2022-10-18 14:38:53 -0700

    Netted loops

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Print _Putchar
+ *
+ * Desription: Print _Putchar
+ * Return: Always return 0
+ */
+
+int _putchar (int har)
+
+{
+	putchar (har);
+
+	return (0);
+}
