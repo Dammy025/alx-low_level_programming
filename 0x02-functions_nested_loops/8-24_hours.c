c4f6b17c1d99d5afeb25ef808bf5764a61fdbdd2
Dammy025 <modupeabimbola55@gmail.com>
2022-10-19 13:53:59 -0700

    Nested loops

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minutes of the day 00:00 to 23:59
+ * @void - no arguments
+ */
+void jack_bauer(void)
+{
+	int a, i;
+
+	for (a = 0; a < 24; a++)
+	{
+		for (i = 0; i < 60; i++)
+		{
+			_putchar(a / 10 + '0');
+			_putchar(a % 10 + '0');
+			_putchar(':');
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
