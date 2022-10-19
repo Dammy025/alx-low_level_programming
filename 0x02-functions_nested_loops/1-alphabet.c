ef9ed79983f7ef3c8b0fbeefcf334cd83093dad1
Dammy025 <modupeabimbola55@gmail.com>
2022-10-19 04:08:47 -0700

    Nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - prints all alphabets in lowercase.
+ * Return: On success 1.
+ */
+void print_alphabet(void)
+{
+	int a = 97;
+	int z = 122;
+	int let = '\n';
+	while (a <= z )
+	{
+		putchar(a);
+	       	a++;
+	}
+	putchar(let);
+}
