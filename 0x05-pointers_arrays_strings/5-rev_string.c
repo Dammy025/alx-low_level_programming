d98612a6a0685c74c44767ca79a8dc1c0fe9a26f
Dammy025 <modupeabimbola55@gmail.com>
2022-10-25 21:35:12 -0700

    C is hard

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,22 +1,24 @@
 #include "main.h"
 /**
- * rev_string - a function that reverses a string
- * @s: input to reverse 
+ * rev_string -a function that reverses a string
+ * @s: input to reverse
  * Return: string and in reverse
  */
-void rev_string(char *s);
+void rev_string(char *s)
 {
-	char rev = s[0];
+char rev = s[0];
 
-	int fcounter = 0;
-	int i;
-	while (s[fcounter] != '\0')
-		fcounter++;
-	for (i = 0; i < fcounter; i++)
-	{
-		fcounter--;
-		rev = s[i];
-		s[i] = s[fcounter];
-		s[fcounter] = rev;
-	}
+int fcounter = 0;
+int i;
+while (s[fcounter] != '\0')
+{
+fcounter++;
+}
+for (i = 0; i < fcounter; i++)
+{
+fcounter__;
+rev = s[i];
+s[i] = s[fcounter];
+s[fcounter] = rev;
+}
 }
