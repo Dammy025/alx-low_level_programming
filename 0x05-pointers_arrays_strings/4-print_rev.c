4ddb83d7f01c02a704c36585518579c933410323
Dammy025 <modupeabimbola55@gmail.com>
2022-10-25 21:26:20 -0700

    C is hard

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,15 +6,16 @@
  */
 void print_rev(char *s)
 {
-	int i = 0;
+int i = 0;
 
-	while (s[i] != 0)
-	{
-		i++;
-	}
-	while (i > 0)
-	{
-		_putchar(s[i]);
-	}
-	_putchar('\n');
+while (s[i] != 0)
+{
+i++;
+}
+while (i > 0)
+{
+i--;
+_putchar(s[i]);
+}
+_putchar('\n');
 }
