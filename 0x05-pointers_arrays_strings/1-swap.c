e1c5c4fd261ba0c24724cca3883af5775c826247
Dammy025 <modupeabimbola55@gmail.com>
2022-10-25 20:41:31 -0700

    C is hard

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,7 +3,7 @@
  * swap_int - a function that swaps the value of two integers
  * @n: takes (int a, b)
  */
-void swap_int(int *a, int *b);
+void swap_int(int *a, int *b)
 {
 	int c = *a;
 	*a = *b;
