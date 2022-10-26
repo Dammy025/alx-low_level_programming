94dc0f4145260170167faf2d2c4f04e311f1e2fb
Dammy025 <modupeabimbola55@gmail.com>
2022-10-25 21:17:48 -0700

    C is hard

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,13 +6,13 @@
  */
 int _strlen(char *s)
 {
-	int i = 1, sum = 0;
+int i = 1, sum = 0;
+char p1 = s[0];
 
-	char p1 = s[0];
-	while (p1 != '\0')
-	{
-		sum++;
-		p1 = s[i++];
-	}
-	return (sum);
+while (p1 != '\0')
+{
+sum++;
+p1 = s[i++];
+}
+return (sum);
 }
